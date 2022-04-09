aaf1106ee973d58ae3b508885229f34c58459fde
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-04-09 16:13:27 +0000

    [add]notification.rb

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,15 @@
 class Notification < ApplicationRecord
+  default_scope -> { order(created_at: :desc) }
+  belongs_to :article, optional: true
+  belongs_to :post_comment, optional: true
+  
+  belongs_to :visitor, class_name: 'User', optional: true
+  belongs_to :visited, class_name: 'User', optional: true
+  
+  ACTION_VALUES = ["favorite", "comment", "follow"].freeze
+
+  validates :visitor_id, presence: true
+  validates :visited_id, presence: true
+  validates :action,  presence: true, inclusion: { in: ACTION_VALUES }
+  validates :checked, inclusion: { in: [true, false] }
 end
