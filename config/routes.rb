de9bc30ec6de95288ecee3e9bcbfb6d3c3b404da
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-04-12 15:38:05 +0000

    [Add]新規登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
 }
-  
+
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  
+
   root to: 'homes#top'
   get "home/about" => "homes#about", as: 'about'
   get "home/diagnosis" => "homes#diagnosis", as: 'diagnosis'
-  
+
   resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
@@ -20,14 +20,14 @@ Rails.application.routes.draw do
     get 'unsubscribe', to: 'users#unsubscribe', as: 'unsubscribe'
     patch 'withdraw', to: 'users#withdraw', as: 'withdraw'
   end
-  
+
   resources :articles, only: [:index, :show, :edit, :new, :create, :destroy, :update] do
     resource :favorites, only: [:create, :destroy, :show]
     resources :post_comments, only: [:create, :destroy]
   end
-  
+
   resources :notifications, only: [:index, :destroy]
-  
+
   resources :chats, only: [:index, :show, :create]
 
   # 問い合わせ
@@ -35,6 +35,6 @@ Rails.application.routes.draw do
   post 'contacts/confirm', to: 'contacts#confirm', as: 'confirm'
   post 'contacts/back', to: 'contacts#back', as: 'back'
   get 'done', to: 'contacts#done', as: 'done'
-  
-  
+
+
 end
