1d7a1ec72693664d68ce02c0a4f8605263ff7cb2
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-04-13 08:11:08 +0000

    [Add]ユーザー新規登録機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top, :about, :diagnosis]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
+
+  def after_sign_out_path_for(resource)
+    top_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name_kana, :nickname, :gender, :age, :partner_gender, :partner_age, :partner_type, :personal_comment, :telephone_number, :address, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :name_kana, :nickname, :gender, :age, :partner_gender, :partner_age, :partner_type, :personal_comment, :telephone_number, :address, :image])
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,26 +9,27 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
 
-  root to: 'homes#top'
-  get "home/about" => "homes#about", as: 'about'
-  get "home/diagnosis" => "homes#diagnosis", as: 'diagnosis'
-
-  resources :users, only: [:index, :show, :edit, :update] do
-    resource :relationships, only: [:create, :destroy]
-    get 'followings' => 'relationships#followings', as: 'followings'
-    get 'followers' => 'relationships#followers', as: 'followers'
-    get 'unsubscribe', to: 'users#unsubscribe', as: 'unsubscribe'
-    patch 'withdraw', to: 'users#withdraw', as: 'withdraw'
+  scope module: :public do
+    get '/top' =>  'homes#top'
+    get '/about' => 'homes#about'
+    get "/diagnosis" => "homes#diagnosis", as: 'diagnosis'
+    resources :users, only: [:index, :show, :edit, :update] do
+      resource :relationships, only: [:create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
+      get 'unsubscribe', to: 'users#unsubscribe', as: 'unsubscribe'
+      patch 'withdraw', to: 'users#withdraw', as: 'withdraw'
+      get 'calendar', to: 'users#calendar', as: 'calendar'
+      get 'timeline', to: 'users#timeline', as: 'timeline'
+      get 'diet', to: 'users#diet', as: 'diet'
+    end
+    resources :articles, only: [:index, :show, :edit, :new, :create, :destroy, :update] do
+      resource :favorites, only: [:create, :destroy, :show]
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :notifications, only: [:index, :destroy]
+    resources :chats, only: [:index, :show, :create]
   end
-
-  resources :articles, only: [:index, :show, :edit, :new, :create, :destroy, :update] do
-    resource :favorites, only: [:create, :destroy, :show]
-    resources :post_comments, only: [:create, :destroy]
-  end
-
-  resources :notifications, only: [:index, :destroy]
-
-  resources :chats, only: [:index, :show, :create]
 
   # 問い合わせ
   resources :contacts, only: [:new, :create]
