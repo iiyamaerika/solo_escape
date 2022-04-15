77bf949d518532a9c32444399d745847f7137dc7
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-04-15 13:59:25 +0000

    [Add]投稿一覧ページ

--- app/models/article.rb
+++ app/models/article.rb
@@ -15,4 +15,13 @@ class Article < ApplicationRecord
     article_private: 1
   }
   
+   # 投稿画像がない場合は"no-image-icon.jpg"を表示させる
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no-image-icon.jpg')
+      image.attach(io: File.open(file_path), filename: 'no-image-icon.jpg', content_type: 'image/jpg')
+    end
+    image.variant(resize_to_fill: [width, height]).processed
+  end
+  
 end
